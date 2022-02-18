044e6f2ee12f4079edbb45c01ff956b268c8643c
Bharat549sa <81371017+Bharat549sa@users.noreply.github.com>
2022-02-18 14:24:46 -0500

    Create kotlin.kt

---
+++ kotlin.kt
@@ -0,0 +1,45 @@
+ pages: 320
+  title: HarryPotter
+  author: J.K . Rowling
+  sayHello(itemToGreet: "kotlin")
+val interestingThings = arrayOf(""kotlin", "programming"", "Comic Books")
+
+println(interestingThing[0])
+println(interestingThing[0])
+}
+fun sayHello(itemToGreet: String, itemTogreet:String)= println("$greeting $itemToGreet"){
+
+
+
+}
+
+interestingThings.ForEach(interestinThing -> println[interrstingThings])
+for(interestingThign in interestingThigns){
+  println(intrestingThing)
+}
+
+itnerestingThings.ForeachINdexed{index, interestitngThing -> println("$interestingTign is at index $index")}
+
+class Book {
+ 
+}
+
+var map = mapOf (1 to "a", 2 to "b", 3 to "c")
+map.forEach(key, vale -> println("$key -> $value"))
+
+val intrestedThings = mautabllistOf("kitlin" , "proaggingJ", "comic books")
+interestThings.add("dogs");
+
+
+map.Foreach(value -> println("4key-> $lvalue"))
+
+map.put(4, "d"), put and add modify colelcitons
+
+fun Hello(greeing:String, itemsTogreet: list<String>){
+  itemTogReet.foEAch(itemTogreet-> println("$greeting $itemTogreet")
+  )
+}
+
+sayhello("greeitng; "hi", ""inenrestingThign)":)
+
+//collectiontype to funciton as parameter
